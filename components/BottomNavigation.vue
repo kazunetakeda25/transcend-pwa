5e50a91da3636290fd40e43cb9b4300b87a23bcf
Rey Santana Socorro <reysantana@users.noreply.github.com>
2020-09-21 03:14:30 -0400

    commit 1

---
+++ components/BottomNavigation.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-bottom-navigation
+    v-model="bottomNav"
+    shift
+    class="primary"
+    grow
+    style="border-top-left-radius: 4px;border-top-right-radius: 4px;"
+  >
+    <v-btn min-width="60">
+      <span style="color: white;">Schedule</span>
+      <v-icon color="white">
+        mdi-calendar-text
+      </v-icon>
+    </v-btn>
+
+    <v-btn min-width="60">
+      <span style="color: white;">Jobs for Bid</span>
+      <v-icon color="white" style="font-size: 26px">
+        mdi-layers
+      </v-icon>
+    </v-btn>
+
+    <v-btn min-width="60">
+      <span style="color: white;">Completed</span>
+      <v-icon color="white">
+        mdi-checkbox-multiple-marked
+      </v-icon>
+    </v-btn>
+
+    <v-btn min-width="60">
+      <span style="color: white;">Message</span>
+      <v-icon color="white">
+        mdi-message-text
+      </v-icon>
+    </v-btn>
+
+    <v-btn min-width="60">
+      <span style="color: white;">Profile</span>
+      <v-icon color="white" style="font-size: 28px">
+        mdi-account
+      </v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      bottomNav: 1
+    }
+  },
+
+  computed: {
+    color () {
+      switch (this.bottomNav) {
+        case 0: return 'schedule'
+        case 1: return 'jobs-for-bid'
+        case 2: return 'completed'
+        case 3: return 'message'
+        case 4: return 'profile'
+      }
+
+      return 'jobs-for-bid'
+    }
+  }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      fixed
+      temporary
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      color="primary"
+    >
+      <v-btn icon color="white">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-spacer />
+      <v-toolbar-title v-text="title" style="color:white;"/>
+      <v-spacer />
+      <v-app-bar-nav-icon color="white" @click.stop="drawer = !drawer" />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer fixed app class="transparent">
+      <bottom-navigation />
+    </v-footer>
+  </v-app>
+</template>
+
+<style scoped>
+.transparent {
+  background-color: transparent !important;
+  border-color: transparent !important;
+ }
+</style>>
+
+<script>
+import BottomNavigation from '~/components/BottomNavigation.vue'
+
+export default {
+  components: {
+    BottomNavigation
+  },
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: false,
+      rightDrawer: false,
+      title: 'Jobs For Bid'
+    }
+  }
+}
+</script>
